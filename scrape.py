42930c84cdeddc41efd7d6d2f4d140b928da8ad2
Samson Bitrus <bitruss@gmail.com>
2019-01-28 23:10:11 -0500

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,1 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from time import sleep
+import re
+import pandas as pd
